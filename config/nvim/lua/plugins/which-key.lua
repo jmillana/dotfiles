d417a49c093a1a6811ac255aa5b7b678b63f7087
jmillana <jordi@marimon.tech>
2024-09-27 08:57:02 +0200

    Update which key

--- config/nvim/lua/plugins/which-key.lua
+++ config/nvim/lua/plugins/which-key.lua
@@ -15,51 +15,19 @@ return {
 		}
 		wk.setup(opts)
 		local get_icon = require("jmillana.utils").get_icon
-		wk.register({
-			r = {
-				name = "[R]efactor",
-				_ = "which_key_ignore",
-			},
-			c = {
-				name = "[C]ode",
-				_ = "which_key_ignore",
-			},
-			d = {
-				name = "[D]ocument",
-				_ = "which_key_ignore",
-			},
-			t = {
-				name = "[T]oggle",
-				_ = "which_key_ignore",
-			},
-			w = {
-				name = "[W]orkspace",
-				_ = "which_key_ignore",
-			},
-			P = {
-				name = get_icon("Plugins") .. "[P]lugins",
-				_ = "which_key_ignore",
-			},
-			s = {
-				name = get_icon("Search") .. "[S]earch",
-				_ = "which_key_ignore",
-			},
-			l = {
-				name = get_icon("ActiveLSP") .. "[L]SP",
-				_ = "which_key_ignore",
-			},
-			b = {
-				name = get_icon("Buffers") .. "[B]uffers",
-				_ = "which_key_ignore",
-			},
-			g = {
-				name = get_icon("GoTo") .. "[G]o To",
-				_ = "which_key_ignore",
-			},
-			G = {
-				name = get_icon("GitHub") .. "[G]IT",
-				_ = "which_key_ignore",
-			},
-		}, { prefix = "<leader>" })
+		wk.add({
+			{ "<leader>g", group = "[R]efactor" },
+            { "<leader>c", group = "[C]ode" },
+            { "<leader>d", group = "[D]ocument" },
+            { "<leader>t", group = "[T]oggle" },
+            { "<leader>g", group = " [g]o to"},
+            { "<leader>w", group = "[W]orkspace"},
+            { "<leader>P", group = " [P]lugins" },
+            { "<leader>s", group = " [S]earch" },
+            { "<leader>l", group = " [L]SP" },
+            { "<leader>b", group = " [B]uffers" },
+            { "<leader>G", group = " [G]IT" },
+            { "<leader>r", group = "[R]e name" },
+		})
 	end,
 }
